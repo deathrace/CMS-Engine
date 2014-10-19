dac202ecc16a066e5f0c31ea9797134d1053a55b
Виталик <webmaster@yaruga.me>
2014-10-20 00:10:36 +0400

    Update Core.php

--- Engine/Core.php
+++ Engine/Core.php
@@ -1,19 +1,30 @@
 <?php
 
-class Core 
+namespace Engine;
+
+class Core extends \Engine\AbstructCore
 {
 
     public static $view;
     
     public static function init() {
         
-        
+        self::$view = new \Smarty();
         
     }
     
     public static function Controller() {
         
+        self::$controller(
+            self::$get->action, 
+            self::$get->controller
+        );
         
+    }
+    
+    public static function getAbstructCore() {
+        
+        return self::$Core;
         
     }
 
