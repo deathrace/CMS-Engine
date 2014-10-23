0ec0171e501ad0d505d0d23303c0332e8c19de41
Виталик <webmaster@yaruga.me>
2014-10-24 00:55:09 +0400

    Update index.php

--- index.php
+++ index.php
@@ -4,4 +4,7 @@ include "Engine/Core.php";
 
 \Engine\Core::init();
 \Engine\Core::Controller();
+\Engine\Core::Action();
+\Engine\Core::SPost();
+\Engine\Core::SGet();
 \Engine\Core::$view->display();
